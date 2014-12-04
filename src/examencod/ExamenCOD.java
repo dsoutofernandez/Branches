572fbd63d284cd907e0388828f60d92ca8a4ffc8
dsoutofernandez <dsoutofernandez@eniac05.danielcastelao.org>
2014-12-04 09:31:24 +0100

    Commit 3

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -7,28 +7,28 @@ public class ExamenCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int iN = 11;
-        if (metodoMio(iN)) {
+        int aux = 11;
+        if (MetodoRefactorizado(aux)) {
             System.out.println("Es numero primo");
         } else {
             System.out.println("NO es numero primo");
         }
 
-        for (int x = 2; x < 1000; x++) {
-            if (metodoMio(x)) {
-                System.out.print(x + " ");
+        for (int Contador = 2; Contador < 1000; Contador++) {
+            if (MetodoRefactorizado(Contador)) {
+                System.out.print(Contador + " ");
             }
         }
     }
 
-    public static boolean metodoMio(int variable) {
-        int j = 2;
+    public static boolean MetodoRefactorizado(int varRec) {
+        int var = 2;
         boolean aux = true;
-        while ((aux) && (j != variable)) {
-            if (variable % j == 0) {
+        while ((aux) && (var != varRec)) {
+            if (varRec % var == 0) {
                 aux = false;
             }
-            j++;
+            var++;
         }
         return aux;
     }
